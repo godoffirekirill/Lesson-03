95580c14aeb1e099057f08335afd18de2479bd9b
kirill kurochkin <godoffirekirill@gmail.com>
2024-04-05 19:32:58 +0200

    create new project for lesson 03

---
+++ TMS Lesson 3/TMS Lesson 3UITests/TMS_Lesson_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TMS_Lesson_3UITestsLaunchTests.swift
+//  TMS Lesson 3UITests
+//
+//  Created by Кирилл Курочкин on 05.04.2024.
+//
+
+import XCTest
+
+final class TMS_Lesson_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
